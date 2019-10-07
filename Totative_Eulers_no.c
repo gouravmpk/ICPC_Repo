96e2eef1129b7686f4499c54a99a611a045351a2
gouravmpk <gouravmpk@gmail.com>
2019-10-07 21:47:59 +0530

    Create Totative_Eulers_no.c

---
+++ Totative_Eulers_no.c
@@ -0,0 +1,14 @@
+int gcd(int a, int b)  \\Greatest common divisor
+{ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+  int phi(unsigned int n) 
+{ 
+    unsigned int result = 1; 
+    for (int i = 2; i < n; i++) 
+        if (gcd(i, n) == 1) 
+            result++; 
+    return result; 
+} 
